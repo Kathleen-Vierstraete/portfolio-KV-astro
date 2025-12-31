ef670ea82850eb55a545bb971f012f4ce1071ab0
Kathleen-Vierstraete <ctrlkate89@gmail.com>
2025-12-31 14:23:14 +0100

    feat: updating hero and heroenglish with skills

--- src/partials/Hero.tsx
+++ src/partials/Hero.tsx
@@ -1,17 +1,23 @@
 import { HeroAvatar, HeroSocial, Section } from 'astro-boilerplate-components';
 
 import AvatarKV from '../../public/assets/images/AvatarMaker-removebg-preview.png';
+import Indesign from '../../public/assets/images/languagesIcons/icons8-adobe-indesign-48.png';
 import ChinaFlag from '../../public/assets/images/languagesIcons/icons8-china-48.png';
 import CSSLogo from '../../public/assets/images/languagesIcons/icons8-css3-48.png';
+import Excel from '../../public/assets/images/languagesIcons/icons8-excel-48.png';
 import FranceFlag from '../../public/assets/images/languagesIcons/icons8-france-48.png';
 import HTMLLogo from '../../public/assets/images/languagesIcons/icons8-html-48.png';
+import Illustrator from '../../public/assets/images/languagesIcons/icons8-illustrator-48.png';
 import JSLogo from '../../public/assets/images/languagesIcons/icons8-javascript-48.png';
 import MySQLLogo from '../../public/assets/images/languagesIcons/icons8-mysql-48.png';
+import Photoshop from '../../public/assets/images/languagesIcons/icons8-photoshop-48.png';
 import PHPLogo from '../../public/assets/images/languagesIcons/icons8-php-48.png';
+import Powerpoint from '../../public/assets/images/languagesIcons/icons8-powerpoint-48.png';
 import ReactLogo from '../../public/assets/images/languagesIcons/icons8-react-48.png';
 import SpainFlag from '../../public/assets/images/languagesIcons/icons8-spain-flag-48.png';
 import SymfonyLogo from '../../public/assets/images/languagesIcons/icons8-symfony-48.png';
 import UkFlag from '../../public/assets/images/languagesIcons/icons8-uk-48.png';
+import Word from '../../public/assets/images/languagesIcons/icons8-word-48.png';
 
 const Hero = () => (
   <Section>
@@ -20,9 +26,10 @@ const Hero = () => (
       description={
         <>
           Je suis développeuse FullStack, avec une certaine appétance pour le
-          BackEnd. N'hésitez pas à découvrir mes projets, développés seule ou en
-          équipe, toujours en 100% remote. Je suis disponible pour de nouvelles
-          aventures pleines de code et de challenges. Contact :{' '}
+          BackEnd. Je suis également créatrice multimédia et communication
+          graphique. N'hésitez pas à découvrir mes projets, développés seule ou
+          en équipe. Je suis disponible pour de nouvelles aventures pleines de
+          code et de challenges. Pour me contacter, c'est juste ici :{' '}
           <span className="text-purple-800 hover:underline">
             <a href="mailto:ctrlkate89@gmail.com">ctrlkate89@gmail.com</a>
           </span>
@@ -37,47 +44,94 @@ const Hero = () => (
         />
       }
       socialButtons={
-        <div className="mt-5 grid grid-cols-4 gap-4 md:grid-cols-8">
-          <HeroSocial src={HTMLLogo.src} alt="HTML icon" />
+        <div className="mt-5">
+          <h3 className="mb-4 text-lg font-semibold text-gray-400">
+            Mes Compétences Développement Web :
+          </h3>
+          <div className="mt-5 grid grid-cols-4 gap-4 md:grid-cols-8">
+            <HeroSocial src={HTMLLogo.src} alt="HTML icon" />
 
-          <HeroSocial src={CSSLogo.src} alt="CSS icon" />
+            <HeroSocial src={CSSLogo.src} alt="CSS icon" />
 
-          <HeroSocial src={PHPLogo.src} alt="PHP icon" />
+            <HeroSocial src={PHPLogo.src} alt="PHP icon" />
 
-          <HeroSocial src={JSLogo.src} alt="JavaScript icon" />
+            <HeroSocial src={JSLogo.src} alt="JavaScript icon" />
 
-          <HeroSocial src={SymfonyLogo.src} alt="Symfony icon" />
+            <HeroSocial src={SymfonyLogo.src} alt="Symfony icon" />
 
-          <HeroSocial src={ReactLogo.src} alt="React icon" />
+            <HeroSocial src={ReactLogo.src} alt="React icon" />
 
-          <HeroSocial src={MySQLLogo.src} alt="MySQL icon" />
+            <HeroSocial src={MySQLLogo.src} alt="MySQL icon" />
+          </div>
         </div>
       }
     />
     <HeroAvatar
       title={<></>}
-      description={<>Je parle aussi plusieurs langues</>}
+      description={<></>}
       avatar={<></>}
       socialButtons={
-        <div className="grid grid-cols-4 gap-4 md:grid-cols-8">
-          <div className="flex flex-col items-center">
-            <HeroSocial src={FranceFlag.src} alt="France Flag" />
-            <p>C2</p>
-          </div>
+        <div className="mt-5">
+          <h3 className="mb-4 text-lg font-semibold text-gray-400">
+            Mes Compétences Suite Adobe et Pack Office :
+          </h3>
+          <div className="grid grid-cols-4 gap-4 md:grid-cols-8">
+            <div className="flex flex-col items-center">
+              <HeroSocial src={Illustrator.src} alt="illustrator logo" />
+            </div>
 
-          <div className="flex flex-col items-center">
-            <HeroSocial src={UkFlag.src} alt="UK Flag" />
-            <p>C1</p>
-          </div>
+            <div className="flex flex-col items-center">
+              <HeroSocial src={Photoshop.src} alt="photoshop logo" />
+            </div>
 
-          <div className="flex flex-col items-center">
-            <HeroSocial src={SpainFlag.src} alt="Spain Flag" />
-            <p>B1</p>
-          </div>
+            <div className="flex flex-col items-center">
+              <HeroSocial src={Indesign.src} alt="indesign logo" />
+            </div>
 
-          <div className="flex flex-col items-center">
-            <HeroSocial src={ChinaFlag.src} alt="China Flag" />
-            <p>A2</p>
+            <div className="flex flex-col items-center">
+              <HeroSocial src={Word.src} alt="Word logo" />
+            </div>
+
+            <div className="flex flex-col items-center">
+              <HeroSocial src={Excel.src} alt="Excel logo" />
+            </div>
+
+            <div className="flex flex-col items-center">
+              <HeroSocial src={Powerpoint.src} alt="Powerpoint logo" />
+            </div>
+          </div>
+        </div>
+      }
+    />
+    <HeroAvatar
+      title={<></>}
+      description={<></>}
+      avatar={<></>}
+      socialButtons={
+        <div className="mt-5">
+          <h3 className="mb-4 text-lg font-semibold text-gray-400">
+            Je parle plusieurs langues :
+          </h3>
+          <div className="grid grid-cols-4 gap-4 md:grid-cols-8">
+            <div className="flex flex-col items-center">
+              <HeroSocial src={FranceFlag.src} alt="France Flag" />
+              <p>C2</p>
+            </div>
+
+            <div className="flex flex-col items-center">
+              <HeroSocial src={UkFlag.src} alt="UK Flag" />
+              <p>C1</p>
+            </div>
+
+            <div className="flex flex-col items-center">
+              <HeroSocial src={SpainFlag.src} alt="Spain Flag" />
+              <p>B1</p>
+            </div>
+
+            <div className="flex flex-col items-center">
+              <HeroSocial src={ChinaFlag.src} alt="China Flag" />
+              <p>A2</p>
+            </div>
           </div>
         </div>
       }
--- src/partials/HeroEnglish.tsx
+++ src/partials/HeroEnglish.tsx
@@ -1,17 +1,23 @@
 import { HeroAvatar, HeroSocial, Section } from 'astro-boilerplate-components';
 
 import AvatarKV from '../../public/assets/images/AvatarMaker-removebg-preview.png';
+import Indesign from '../../public/assets/images/languagesIcons/icons8-adobe-indesign-48.png';
 import ChinaFlag from '../../public/assets/images/languagesIcons/icons8-china-48.png';
 import CSSLogo from '../../public/assets/images/languagesIcons/icons8-css3-48.png';
+import Excel from '../../public/assets/images/languagesIcons/icons8-excel-48.png';
 import FranceFlag from '../../public/assets/images/languagesIcons/icons8-france-48.png';
 import HTMLLogo from '../../public/assets/images/languagesIcons/icons8-html-48.png';
+import Illustrator from '../../public/assets/images/languagesIcons/icons8-illustrator-48.png';
 import JSLogo from '../../public/assets/images/languagesIcons/icons8-javascript-48.png';
 import MySQLLogo from '../../public/assets/images/languagesIcons/icons8-mysql-48.png';
+import Photoshop from '../../public/assets/images/languagesIcons/icons8-photoshop-48.png';
 import PHPLogo from '../../public/assets/images/languagesIcons/icons8-php-48.png';
+import Powerpoint from '../../public/assets/images/languagesIcons/icons8-powerpoint-48.png';
 import ReactLogo from '../../public/assets/images/languagesIcons/icons8-react-48.png';
 import SpainFlag from '../../public/assets/images/languagesIcons/icons8-spain-flag-48.png';
 import SymfonyLogo from '../../public/assets/images/languagesIcons/icons8-symfony-48.png';
 import UkFlag from '../../public/assets/images/languagesIcons/icons8-uk-48.png';
+import Word from '../../public/assets/images/languagesIcons/icons8-word-48.png';
 
 const HeroEnglish = () => (
   <Section>
@@ -19,10 +25,10 @@ const HeroEnglish = () => (
       title={<>Hello, my name is Kate</>}
       description={
         <>
-          I am a Fullstack developer, with a preference for the BackEnd. Feel
-          free to discover my projects, developed remotely on my own or with
-          teammates. I am available for new adventures full of code and
-          challenges. Contact :{' '}
+          I am a Fullstack developer, with a preference for the BackEnd. I am
+          also a multimedia content creator. Feel free to discover my projects,
+          developed on my own or with teammates. I am available for new
+          adventures full of code and challenges. You can contact me via email :{' '}
           <span className="text-purple-800 hover:underline">
             <a href="mailto:ctrlkate89@gmail.com">ctrlkate89@gmail.com</a>
           </span>
@@ -37,47 +43,94 @@ const HeroEnglish = () => (
         />
       }
       socialButtons={
-        <div className="mt-5 grid grid-cols-4 gap-4 md:grid-cols-8">
-          <HeroSocial src={HTMLLogo.src} alt="HTML icon" />
+        <div className="mt-5">
+          <h3 className="mb-4 text-lg font-semibold text-gray-400">
+            My Web Developement skills :
+          </h3>
+          <div className="mt-5 grid grid-cols-4 gap-4 md:grid-cols-8">
+            <HeroSocial src={HTMLLogo.src} alt="HTML icon" />
 
-          <HeroSocial src={CSSLogo.src} alt="CSS icon" />
+            <HeroSocial src={CSSLogo.src} alt="CSS icon" />
 
-          <HeroSocial src={PHPLogo.src} alt="PHP icon" />
+            <HeroSocial src={PHPLogo.src} alt="PHP icon" />
 
-          <HeroSocial src={JSLogo.src} alt="JavaScript icon" />
+            <HeroSocial src={JSLogo.src} alt="JavaScript icon" />
 
-          <HeroSocial src={SymfonyLogo.src} alt="Symfony icon" />
+            <HeroSocial src={SymfonyLogo.src} alt="Symfony icon" />
 
-          <HeroSocial src={ReactLogo.src} alt="React icon" />
+            <HeroSocial src={ReactLogo.src} alt="React icon" />
 
-          <HeroSocial src={MySQLLogo.src} alt="MySQL icon" />
+            <HeroSocial src={MySQLLogo.src} alt="MySQL icon" />
+          </div>
         </div>
       }
     />
     <HeroAvatar
       title={<></>}
-      description={<>I speak multiple languages</>}
+      description={<></>}
       avatar={<></>}
       socialButtons={
-        <div className="grid grid-cols-4 gap-4 md:grid-cols-8">
-          <div className="flex flex-col items-center">
-            <HeroSocial src={FranceFlag.src} alt="France Flag" />
-            <p>C2</p>
-          </div>
+        <div className="mt-5">
+          <h3 className="mb-4 text-lg font-semibold text-gray-400">
+            My Suite Adobe and Office Pack skills:
+          </h3>
+          <div className="grid grid-cols-4 gap-4 md:grid-cols-8">
+            <div className="flex flex-col items-center">
+              <HeroSocial src={Illustrator.src} alt="illustrator logo" />
+            </div>
 
-          <div className="flex flex-col items-center">
-            <HeroSocial src={UkFlag.src} alt="UK Flag" />
-            <p>C1</p>
-          </div>
+            <div className="flex flex-col items-center">
+              <HeroSocial src={Photoshop.src} alt="photoshop logo" />
+            </div>
 
-          <div className="flex flex-col items-center">
-            <HeroSocial src={SpainFlag.src} alt="Spain Flag" />
-            <p>B1</p>
-          </div>
+            <div className="flex flex-col items-center">
+              <HeroSocial src={Indesign.src} alt="indesign logo" />
+            </div>
 
-          <div className="flex flex-col items-center">
-            <HeroSocial src={ChinaFlag.src} alt="China Flag" />
-            <p>A2</p>
+            <div className="flex flex-col items-center">
+              <HeroSocial src={Word.src} alt="Word logo" />
+            </div>
+
+            <div className="flex flex-col items-center">
+              <HeroSocial src={Excel.src} alt="Excel logo" />
+            </div>
+
+            <div className="flex flex-col items-center">
+              <HeroSocial src={Powerpoint.src} alt="Powerpoint logo" />
+            </div>
+          </div>
+        </div>
+      }
+    />
+    <HeroAvatar
+      title={<></>}
+      description={<></>}
+      avatar={<></>}
+      socialButtons={
+        <div className="mt-5">
+          <h3 className="mb-4 text-lg font-semibold text-gray-400">
+            I speak several languages :
+          </h3>
+          <div className="grid grid-cols-4 gap-4 md:grid-cols-8">
+            <div className="flex flex-col items-center">
+              <HeroSocial src={FranceFlag.src} alt="France Flag" />
+              <p>C2</p>
+            </div>
+
+            <div className="flex flex-col items-center">
+              <HeroSocial src={UkFlag.src} alt="UK Flag" />
+              <p>C1</p>
+            </div>
+
+            <div className="flex flex-col items-center">
+              <HeroSocial src={SpainFlag.src} alt="Spain Flag" />
+              <p>B1</p>
+            </div>
+
+            <div className="flex flex-col items-center">
+              <HeroSocial src={ChinaFlag.src} alt="China Flag" />
+              <p>A2</p>
+            </div>
           </div>
         </div>
       }
